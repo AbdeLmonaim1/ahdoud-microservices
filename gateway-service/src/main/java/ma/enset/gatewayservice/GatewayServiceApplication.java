72a205d6dc81bed643c4b3786050da0dcc81cefb
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2026-01-02 23:59:39 +0100

    Resolve CORS's problems

--- gateway-service/src/main/java/ma/enset/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/ma/enset/gatewayservice/GatewayServiceApplication.java
@@ -6,6 +6,12 @@ import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -22,4 +28,14 @@ public class GatewayServiceApplication {
         return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
 }
